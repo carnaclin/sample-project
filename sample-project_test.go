0de84c6914907c3c546226f9efe51054a5e5091b
carnaclin <fernando.nara@outlook.com>
2017-01-20 20:51:14 +1100

    testing code for line counter

--- sample-project_test.go
+++ sample-project_test.go
@@ -1,3 +1,30 @@
-package stringutil
+// Test code for sample-project by Fernando Nara
+// 20/01/17
 
-//Test function
+package main
+
+import "testing"
+
+type testpair struct {
+    path string
+    result int
+}
+
+var tests = []testpair{
+    {"C:\Users\ferna\Desktop\gutenberg\2\1\1\2112\2112.txt", 7539},
+    {"C:\Users\ferna\Desktop\gutenberg\6\5\657\657.txt",10570},
+    {"C:\Users\ferna\Desktop\gutenberg\6\9\8\6984\6984.txt",5776},
+}
+
+func TestcountLines(t *testing.T) {
+	for _; pair := range tests {
+        lineCount := countLines(pair.path, outputFile)
+        if linecount != pair.result {
+            t.Error {
+                "For", filepath.Base(pair.path),
+                "expected", pair.result
+                "got", lineCount,frefrefrefer 
+            }
+        }
+    }
+}
