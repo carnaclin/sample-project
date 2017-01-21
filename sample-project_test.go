537b4a201f37ee2fc4f36ac353256624203f7efa
carnaclin <fernando.nara@outlook.com>
2017-01-21 16:14:36 +1100

    update

--- sample-project_test.go
+++ sample-project_test.go
@@ -3,28 +3,31 @@
 
 package main
 
-import "testing"
+import (
+	"path/filepath"
+	"testing"
+)
 
-type testpair struct {
-    path string
-    result int
+type testdata struct {
+	path   string
+	result int
 }
 
-var tests = []testpair{
-    {"C:\Users\ferna\Desktop\gutenberg\2\1\1\2112\2112.txt", 7539},
-    {"C:\Users\ferna\Desktop\gutenberg\6\5\657\657.txt",10570},
-    {"C:\Users\ferna\Desktop\gutenberg\6\9\8\6984\6984.txt",5776},
+var tests = []testdata{
+	{"test1.txt", 7539},
+	{"test2.txt", 10570},
+	{"test3.txt", 5776},
 }
 
 func TestcountLines(t *testing.T) {
-	for _; pair := range tests {
-        lineCount := countLines(pair.path, outputFile)
-        if linecount != pair.result {
-            t.Error {
-                "For", filepath.Base(pair.path),
-                "expected", pair.result
-                "got", lineCount,frefrefrefer 
-            }
-        }
-    }
+	for _, pair := range tests {
+		lineCount := countLines(pair.path)
+		if lineCount != pair.result {
+			t.Error(
+				"For", filepath.Base(pair.path),
+				"expected", pair.result,
+				"got", lineCount,
+			)
+		}
+	}
 }
