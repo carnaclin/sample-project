eceea9dfe136b6237b36008b2f34df6c79f5967f
carnaclin <fernando.nara@outlook.com>
2017-01-22 23:35:50 +1100

    added wordcounter

--- sample-project.go
+++ sample-project.go
@@ -35,12 +35,10 @@ func processTxtFiles(outputFile *os.File, word string) filepath.WalkFunc {
 
 // Count lines and print results for each file found
 func countLines(path string, word string) (int, int) {
-	// Implement word counter with line counter
 	inputFile, err := os.Open(path)
 	check(err)
 	defer inputFile.Close()
 	fileScanner := bufio.NewScanner(inputFile)
-	//fileScanner.Split(bufio.ScanLines) //already by default
 	lineCount, wordCount := 0, 0
 	var textLine string
 	for fileScanner.Scan() {
@@ -48,38 +46,25 @@ func countLines(path string, word string) (int, int) {
 		textLine = fileScanner.Text()
 		wordCount += countWords(textLine, word)
 	}
-	//fmt.Printf("number of lines %d\n", lineCount)
-	//fmt.Printf("number of words %d\n", wordCount)
 	return lineCount, wordCount
 }
 
-// Count words
+// Count words occurrences one line at a time
 func countWords(textLine string, word string) int {
-	// Here i get a text line as string to work with. I need to tokenize it correctly
-	// to get all the words and do map["word"]++ to count numbers of "words"
-
-	// src is the input to tokenize.
 	src := []byte(textLine)
-
-	// Initialize the scanner.
 	var s scanner.Scanner
 	fset := token.NewFileSet()                      // positions are relative to fset
 	file := fset.AddFile("", fset.Base(), len(src)) // register input "file"
 	s.Init(file, src, nil /* no error handler */, scanner.ScanComments)
-
-	// Repeated calls to san yield the token sequence found in the input.
 	wordCount := 0
 	for {
 		_, tok, lit := s.Scan()
 		if strings.ToLower(lit) == word {
 			wordCount++
-			//fmt.Println("\t\t\tMatch")
-			//fmt.Printf("\t\t\twordCount = %d\n", wordCount)
 		}
 		if tok == token.EOF {
 			break
 		}
-		//fmt.Printf("%s\t%s\t%q\n", fset.Position(pos), tok, lit)
 	}
 	return wordCount
 }
